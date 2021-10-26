a91ae91a7f99bf97fc4fcc60de0f29fd61ea16a4
Meysow <Meysow@users.noreply.github.com>
2021-10-26 14:36:30 +0400

    Updates

--- src/components/hooks/Viewport.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ViewPortContext } from "./MyContexts";
-
-const useViewport = () => {
-  const { width } = React.useContext(ViewPortContext);
-  return { width };
-};
-
-export default useViewport;
